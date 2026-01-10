2e6910aa8c4711dd2dc088b18e2995792de4456c
Sathya-1307 <2312049@nec.edu.in>
2026-01-10 22:15:02 +0530

    email updated for coordinator

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const dotenv = require('dotenv');
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
- app.use(cors());
+ //app.use(cors());
 //SSO ROUTES
 const webinarSSORoutes = require('./single-sign-on/routes/webinar');
 app.use('/api/nec', webinarSSORoutes);
@@ -76,7 +76,7 @@ const companyMappingRoutes = require('./routes/companyMapping');
 // Middleware 
 
 //Cors For Producion
-// app.use(cors({ origin: ["https://necalumni.nec.edu.in", "https://necalumni.nec.edu.in/alumnimain"], credentials: true }));
+app.use(cors({ origin: ["https://necalumni.nec.edu.in", "https://necalumni.nec.edu.in/alumnimain"], credentials: true }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
